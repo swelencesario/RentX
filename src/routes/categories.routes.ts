5eb5eadae578b1cf5145ee6cbf5f369e3b7d5ef1
Swelen Cesario Ebert <swelens2@hotmail.com>
2021-06-03 15:25:12 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,26 @@
+import { request, response, Router } from 'express';
+
+import { PostgresCategoriesRepository} from "../repositories/PostgresCategoriesRepository"
+import { CreateCategoryService } from '../services/CreateCategoryService';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new PostgresCategoriesRepository();
+
+
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;    
+
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+    createCategoryService.execute({ name, description });
+
+    return response.status(201).send();  
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
+})
+
+export { categoriesRoutes }
