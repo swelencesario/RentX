379055421c0d892c51d902c386bd465a681f0a56
Swelen Cesario Ebert <swelens2@hotmail.com>
2021-06-08 15:52:50 -0300

    leitura de arquivos

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -20,13 +20,13 @@ class ImportCategoryUseCase {
             stream.pipe(parseFile);
 
             parseFile
-            .on("data", async (line) => {
-                const [name, description] = line;
-                categories.push({
-                    name,
-                    description
-                });
-            })
+                .on("data", async (line) => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description
+                    });
+                })
                 .on("end", () => {
                     resolve(categories);
                 })
@@ -37,7 +37,19 @@ class ImportCategoryUseCase {
     }
     async execute(file: Express.Multer.File): Promise<void> {
         const categories = await this.loadCategories(file);
-        console.log(categories);
-    };
+
+        categories.map(async (category) => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description
+                });
+            }
+        });
+    }
 }
 export { ImportCategoryUseCase };
